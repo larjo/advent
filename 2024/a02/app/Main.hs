a482e5077e8915dfffcb0b91fb4f7b72734d23c2
Lars <lars.baker@uc.se>
2024-12-03 00:16:44 +0100

    refactor

--- 2024/a02/app/Main.hs
+++ 2024/a02/app/Main.hs
@@ -1,7 +1,10 @@
 module Main where
 
+pairs :: [a] -> [(a, a)]
+pairs = zip <*> tail
+
 diffs :: [Int] -> [Int]
-diffs = map (uncurry (-)) . (zip <*> tail)
+diffs = map (uncurry (-)) . pairs
 
 inRange :: Int -> Int -> [Int] -> Bool
 inRange a b = all (\x -> a <= x && x <= b)
@@ -12,4 +15,4 @@ isOk xs = inRange 1 3 xs || inRange (-3) (-1) xs
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print $ length $ filter isOk $ map (diffs . map read . words) $ lines input
+  print . length . filter isOk . map (diffs . map read . words) . lines $ input
