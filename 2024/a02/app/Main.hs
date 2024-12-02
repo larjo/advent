2d57bcbf095c207820c707541840f0645557491e
Lars <lars.baker@uc.se>
2024-12-03 00:33:13 +0100

    Clean up

--- 2024/a02/app/Main.hs
+++ 2024/a02/app/Main.hs
@@ -1,4 +1,5 @@
 module Main where
+
 import Control.Applicative (liftA2)
 
 diffs :: [Int] -> [Int]
@@ -7,11 +8,10 @@ diffs = zipWith (-) <*> tail
 inRange :: Int -> Int -> [Int] -> Bool
 inRange a b = all (\x -> a <= x && x <= b)
 
-(<||>) :: ([Int] -> Bool) -> ([Int] -> Bool) -> [Int] -> Bool
-(<||>) = liftA2 (||)
-
 isOk :: [Int] -> Bool
 isOk = inRange 1 3 <||> inRange (-3) (-1)
+  where
+    (<||>) = liftA2 (||)
 
 main :: IO ()
 main = do
