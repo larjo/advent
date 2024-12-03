9ebc4e58b346b08965ea07ddb2435e561b8849d4
Lars <lars.baker@uc.se>
2024-12-03 19:56:46 +0100

    Finish part 2

--- 2024/a02/app/Main.hs
+++ 2024/a02/app/Main.hs
@@ -5,13 +5,29 @@ import Control.Applicative (liftA2)
 diffs :: [Int] -> [Int]
 diffs = zipWith (-) <*> tail
 
+between :: (Ord a) => a -> a -> a -> Bool
+between a b x = a <= x && x <= b
+
+(<||>) :: (Applicative f) => (f a -> Bool) -> (f a -> Bool) -> f a -> Bool
+(<||>) = liftA2 (||)
+
 allInRange :: [Int] -> Bool
-allInRange = all (between 1 3) <||> all (between (-3) (-1))
-  where
-    between a b x = a <= x && x <= b
-    (<||>) = liftA2 (||)
+allInRange = (all (between 1 3) <||> all (between (-3) (-1))) . diffs
+
+removeOne :: [a] -> [[a]]
+removeOne [] = []
+removeOne (x : xs) = xs : map (x :) (removeOne xs)
+
+maxOneOutsideRange :: [Int] -> Bool
+maxOneOutsideRange ints =
+  allInRange ints || any allInRange (removeOne ints)
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  print . length . filter allInRange . map (diffs . map read . words) . lines $ input
+
+  putStr "Part 1: "
+  print . length . filter allInRange . map (map read . words) . lines $ input
+
+  putStr "Part 2: "
+  print . length . filter maxOneOutsideRange . map (map read . words) . lines $ input
