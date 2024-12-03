5c71c61fc24d5549b6ff32742a05c12397b83cfc
Lars <lars.baker@uc.se>
2024-12-03 21:03:30 +0100

    Done

--- 2024/a02/app/Main.hs
+++ 2024/a02/app/Main.hs
@@ -6,15 +6,15 @@ import Data.List (inits, tails)
 diffs :: [Int] -> [Int]
 diffs = zipWith (-) <*> tail
 
+removeOne :: [a] -> [[a]]
+removeOne = liftA2 (zipWith (++)) inits (tail . tails)
+
 allInRange :: [Int] -> Bool
 allInRange =
   (all (between 1 3) <||> all (between (-3) (-1))) . diffs
   where
     between a b x = a <= x && x <= b
     (<||>) = liftA2 (||)
-
-removeOne :: [a] -> [[a]]
-removeOne = liftA2 (zipWith (++)) inits (tail . tails)
 
 maxOneOutsideRange :: [Int] -> Bool
 maxOneOutsideRange = any allInRange <$> removeOne
@@ -24,7 +24,9 @@ main = do
   input <- readFile "input.txt"
 
   putStr "Part 1: "
-  print . length . filter allInRange . map (map read . words) . lines $ input
+  print $ count allInRange input
 
   putStr "Part 2: "
-  print . length . filter maxOneOutsideRange . map (map read . words) . lines $ input
+  print $ count maxOneOutsideRange input
+  where
+    count f = length . filter f . map (map read . words) . lines
