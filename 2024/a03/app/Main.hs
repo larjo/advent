bc45783456782b0d17d9b0961c7fef89ae840db1
Lars <lars.baker@uc.se>
2024-12-03 22:52:53 +0100

    Simplify

--- 2024/a03/app/Main.hs
+++ 2024/a03/app/Main.hs
@@ -1,48 +1,41 @@
 module Main where
 
+import Data.Void (Void)
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import Data.Void ( Void )
 
 type Parser = Parsec Void String
 
-helloParser :: Parser String
-helloParser = string "hello"
-
 numberParser :: Parser Int
 numberParser = do
-    a <- count' 1 3 digitChar
-    return $ read a
+  a <- count' 1 3 digitChar
+  return $ read a
 
 mulParser :: Parser Int
 mulParser = do
-    _ <- string "mul"
-    _ <- char '('
-    a <- numberParser
-    _ <- char ','
-    b <- numberParser
-    _ <- char ')'
-    return $ a * b
+  _ <- string "mul"
+  _ <- char '('
+  a <- numberParser
+  _ <- char ','
+  b <- numberParser
+  _ <- char ')'
+  return $ a * b
 
-
--- Modified findAllOccurrences function
-findAllOccurrences :: Parser a -> Parser [a]
-findAllOccurrences p = go []
+findAllOccurrences :: Parser Int -> Parser Int
+findAllOccurrences p = go 0
   where
     go acc = do
       end <- atEnd
       if end
-        then return (reverse acc)
+        then return acc
         else do
           res <- optional (try p)
           case res of
-            Just val -> go (val : acc)
-            Nothing  -> anySingle *> go acc
+            Just val -> go (val + acc)
+            Nothing -> anySingle *> go acc
 
 main :: IO ()
 main = do
-    input <- readFile "input.txt"
-    putStrLn input
-    let a = parseMaybe (findAllOccurrences mulParser) input
-    print $ sum <$> a
-
+  input <- readFile "input.txt"
+  let s = parseMaybe (findAllOccurrences mulParser) input
+  mapM_ print s
