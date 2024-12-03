babe3c80cdd9627944a25e9a06e5a0176ac13513
Lars Jonsson Baker <lars.o.baker@gmail.com>
2024-12-03 08:47:21 +0100

    Refactor (wip)

--- 2024/a02/app/Main.hs
+++ 2024/a02/app/Main.hs
@@ -5,12 +5,10 @@ import Control.Applicative (liftA2)
 diffs :: [Int] -> [Int]
 diffs = zipWith (-) <*> tail
 
-inRange :: Int -> Int -> [Int] -> Bool
-inRange a b = all (\x -> a <= x && x <= b)
-
 isOk :: [Int] -> Bool
-isOk = inRange 1 3 <||> inRange (-3) (-1)
+isOk = allBetween 1 3 <||> allBetween (-3) (-1)
   where
+    allBetween a b = all (\x -> a <= x && x <= b)
     (<||>) = liftA2 (||)
 
 main :: IO ()
