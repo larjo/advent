cada4ae7dd2bc9481fcba9f888bffa08c151af2e
Lars <lars.baker@uc.se>
2024-12-09 07:57:45 +0100

    wip

--- 2024/a03/app/Main.hs
+++ 2024/a03/app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
+import Text.Megaparsec.State (initialState)
 
 type Parser = Parsec Void String
 
@@ -33,6 +34,21 @@ findAllOccurrences p = go 0
           case res of
             Just val -> go (val + acc)
             Nothing -> anySingle *> go acc
+
+test :: IO ()
+test = do
+  let inp = "mul(2,3)xmul(3,4)y"
+  let state0 = initialState "nofile" inp :: State String Void
+  let (state1, _res1) = runParser' mulParser state0
+  let state2 = state1 { stateInput = drop 1 (stateInput state1) }
+  let (state3, res3) = runParser' mulParser state2
+
+  putStrLn $ "state3: " ++ show state3
+  putStrLn $ "res3: " ++ show res3
+
+
+-- manyMuls :: Parser [Int]
+-- manyMuls = try mulParser <|> single alphaNumChar
 
 main :: IO ()
 main = do
