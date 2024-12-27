c1d6373c99cf8a562affcd0947d552c27c09f52f
Lars <lars.baker@uc.se>
2024-12-28 00:26:21 +0100

    Add part 2

--- 2024/a03/app/Main.hs
+++ 2024/a03/app/Main.hs
@@ -1,10 +1,8 @@
 module Main where
 
-import Data.Functor.Identity (Identity)
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import Text.Megaparsec.State (initialState)
 
 type Parser = Parsec Void String
 
@@ -12,6 +10,12 @@ numberParser :: Parser Int
 numberParser = do
   a <- count' 1 3 digitChar
   return $ read a
+
+data MulState = MulState
+  { acc :: Int,
+    enabled :: Bool
+  }
+  deriving (Show)
 
 mulParser :: Parser Int
 mulParser = do
@@ -23,34 +27,51 @@ mulParser = do
   _ <- char ')'
   return $ a * b
 
-findAllOccurrences :: Parser Int -> Parser Int
-findAllOccurrences p = go 0
-  where
-    go acc = do
-      end <- atEnd
-      if end
-        then return acc
-        else do
-          res <- optional (try p)
-          case res of
-            Just val -> go (val + acc)
-            Nothing -> anySingle *> go acc
+doParser :: MulState -> Parser MulState
+doParser mulState = do
+  _ <- string "do()"
+  return mulState { enabled = True }
 
-parseAll :: Parser Int -> Parser Int
-parseAll p = getParserState >>= go 0
+dontParser :: MulState -> Parser MulState
+dontParser mulState = do
+  _ <- string "don't()"
+  return mulState { enabled = False }
+
+mulStateParser :: MulState -> Parser MulState
+mulStateParser mulState = do
+  val <- mulParser
+  if enabled mulState
+    then return $ mulState {acc = acc mulState + val}
+    else return mulState
+
+parsePart1 :: Parser Int
+parsePart1 = getParserState >>= go 0
   where
-    go acc state =
+    go prod state =
       case stateInput state of
-        [] -> return acc
+        [] -> return prod
         _ ->
-          let (nextState, res) = runParser' p state in
+          let (nextState, res) = runParser' mulParser state in
           case res of
-            Right val -> go (val + acc) nextState
-            Left _ -> go acc (state {stateInput = tail (stateInput state)})
+            Right val -> go (val + prod) nextState
+            Left _ -> go prod (state {stateInput = tail (stateInput state)})
+
+parsePart2 :: Parser MulState
+parsePart2 = getParserState >>= go (MulState 0 True)
+  where
+    go mulState state =
+      case stateInput state of
+        [] -> return mulState
+        _ ->
+          let (nextState, res) = runParser' (mulStateParser mulState <|> doParser mulState <|> dontParser mulState) state
+           in case res of
+                Right val -> go val nextState
+                Left _ -> go mulState (state {stateInput = tail (stateInput state)})
 
 main :: IO ()
 main = do
   input <- readFile "input.txt"
   putStr "Part 1: "
-  parseTest (parseAll mulParser) input
-  parseTest (findAllOccurrences mulParser) input
+  parseTest parsePart1 input
+  putStr "Part 2: "
+  parseTest parsePart2 input
