37099787942b9e56a7f1bee0518fcf6986326bdf
Lars <lars.baker@uc.se>
2024-12-29 23:59:05 +0100

    Add array definition

--- 2024/a04/app/Main.hs
+++ 2024/a04/app/Main.hs
@@ -1,4 +1,20 @@
 module Main where
 
+import Data.Array (Array, array, (!))
+type CharArray = Array (Int, Int) Char
+
+mkCharArray :: [String] -> CharArray
+mkCharArray l =
+  array ((0, 0), (xMax, yMax)) [((i, j), (l !! j) !! i) | i <- [0 .. xMax], j <- [0 .. yMax]]
+  where
+    xMax = length (head l) - 1
+    yMax = length l - 1
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  input <- readFile "input.txt"
+  let ca = mkCharArray $ lines input
+  print $ ca ! (0, 0)
+  print $ ca ! (9, 0)
+  print $ ca ! (0, 9)
+  print $ ca ! (9, 9)
