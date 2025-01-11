782340e429bdf1cb43eff9377e144c41571a32bc
Lars <lars.baker@uc.se>
2025-01-11 23:20:28 +0100

    wip

--- 2024/a06/app/Main.hs
+++ 2024/a06/app/Main.hs
@@ -24,17 +24,15 @@ data Guard = Guard
 charAt :: CharArray -> Vec -> Char
 charAt labMap (Vec p _) = labMap ! p
 
+stopped :: Vec -> Bool
+stopped (Vec _ (0, 0)) = True
+stopped _ = False
+
 outside :: CharArray -> Vec -> Bool
 outside labMap (Vec (x, y) _) =
   x < xMin || x > xMax || y < yMin || y > yMax
   where
     ((xMin, yMin), (xMax, yMax)) = bounds labMap
-
-fix :: (Guard -> Guard) -> Guard -> Guard
-fix f x =
-  if (dir . vec $ x) == (0, 0)
-    then x
-    else fix f (f x)
 
 rotateIndex :: Index -> Index
 rotateIndex (x, y) = (-y, x)
@@ -49,46 +47,42 @@ mkCharArray l =
     xMax = length (head l) - 1
     yMax = length l - 1
 
-guardStart :: Char -> CharArray -> Guard
-guardStart c labMap =
-  Guard startVec [startVec]
+startVec :: Char -> CharArray -> Vec
+startVec c labMap =
+  Vec startPos (0, -1)
   where
     startPos = head [i | i <- range (bounds labMap), labMap ! i == c]
-    startVec = Vec startPos (0, -1)
 
 forward :: Vec -> Vec
 forward (Vec curPos curDir) = Vec (curPos ^+^ curDir) curDir
 
-move :: CharArray -> Guard -> Guard
-move labMap (Guard curVec curHist)
-  | outside labMap nextVec = Guard (curVec { dir = (0, 0) }) curHist
-  | charAt labMap nextVec == '#' = move labMap $ Guard (rotate curVec) curHist
-  | otherwise = Guard nextVec (nextVec : curHist)
+move :: CharArray -> Vec -> Vec
+move labMap curVec
+  | outside labMap nextVec = curVec { dir = (0, 0) }
+  | charAt labMap nextVec == '#' = move labMap . rotate $ curVec
+  | otherwise = nextVec
   where
     nextVec = forward curVec
 
 candidates :: CharArray -> Vec -> [Vec]
-candidates labMap =
-  takeWhile isCandidate . drop 1 . iterate forward . rotate
-  where
-    isCandidate v = not (outside labMap v) && (charAt labMap v /= '#')
+candidates labMap = drop 1 . iterate (move labMap) . rotate
 
 possibleObstruction :: CharArray -> [Vec] -> Bool
 possibleObstruction labMap partHistory =
   not . null $ cs `intersect` partHistory -- && traceShow (forward startVec) True
   where
-    startVec = head partHistory
-    cs = candidates labMap startVec
+    start = head partHistory
+    cs = candidates labMap start
 
 test :: IO ()
 test = do
   input <- readFile "test-input.txt"
   let labMap = mkCharArray . lines $ input
   let partHistory = [Vec {pos = (4,6), dir = (0,-1)},Vec {pos = (4,5), dir = (0,-1)},Vec {pos = (4,4), dir = (0,-1)},Vec {pos = (4,3), dir = (0,-1)},Vec {pos = (4,2), dir = (0,-1)},Vec {pos = (4,1), dir = (0,-1)},Vec {pos = (5,1), dir = (1,0)},Vec {pos = (6,1), dir = (1,0)},Vec {pos = (7,1), dir = (1,0)},Vec {pos = (8,1), dir = (1,0)},Vec {pos = (8,2), dir = (0,1)},Vec {pos = (8,3), dir = (0,1)},Vec {pos = (8,4), dir = (0,1)},Vec {pos = (8,5), dir = (0,1)},Vec {pos = (8,6), dir = (0,1)},Vec {pos = (7,6), dir = (-1,0)},Vec {pos = (6,6), dir = (-1,0)},Vec {pos = (5,6), dir = (-1,0)},Vec {pos = (4,6), dir = (-1,0)},Vec {pos = (3,6), dir = (-1,0)},Vec {pos = (2,6), dir = (-1,0)},Vec {pos = (2,5), dir = (0,-1)}]
-  let startVec = head partHistory
-  let cs = candidates labMap startVec
+  let start = head partHistory
+  let cs = candidates labMap start
   let ok = not . null $ cs `intersect` partHistory
-  print startVec
+  print start
   print cs
   print ok
 
@@ -96,12 +90,12 @@ main :: IO ()
 main = do
   input <- readFile "input.txt"
   let labMap = mkCharArray . lines $ input
-  let start = guardStart '^' labMap
+  let path = takeWhile (not . stopped) . iterate (move labMap)
+  let start = startVec '^' labMap
 
   putStr "Part 1: "
-  let history = reverse . hist . fix (move labMap) $ start
-  print $ length . nub . map pos $ history
-
-  putStr "Part 2: "
-  print $ length . filter (possibleObstruction labMap . reverse) . tail . inits $ history
+  print $ length . nub . map pos . path $ start
+-- 
+--   putStr "Part 2: "
+--   print $ length . filter (possibleObstruction labMap . reverse) . tail . inits $ history
   -- 503 is too low
