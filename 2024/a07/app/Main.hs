2748cf5143f09c06ca99d0ee92b66c5fe455bb0d
Lars <lars.baker@uc.se>
2025-01-31 00:04:59 +0100

    parse input

--- 2024/a07/app/Main.hs
+++ 2024/a07/app/Main.hs
@@ -1,4 +1,31 @@
 module Main where
 
+import Data.Void ( Void )
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void String
+
+data Equation = Equation {
+    res :: Int,
+    numbers :: [Int]
+} deriving Show
+
+equationParser :: Parser Equation
+equationParser = Equation <$> L.decimal <* string ": " <*> L.decimal `sepBy` char ' '
+
+test :: Parser Int
+test = L.decimal
+
+inputParser :: Parser [Equation]
+inputParser = many (equationParser <* eol)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    input <- readFile "test-input.txt"
+    let x = parse inputParser "" input
+    case x of
+        Left err -> putStrLn $ "Error: " ++ errorBundlePretty err
+        Right n  -> mapM_ print n
+    
