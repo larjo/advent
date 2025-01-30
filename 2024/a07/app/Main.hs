691adbd40497be38c429d6dd3cc90a6b8f5d7049
Lars <lars.baker@uc.se>
2025-01-31 00:38:03 +0100

    Formatting

--- 2024/a07/app/Main.hs
+++ 2024/a07/app/Main.hs
@@ -1,31 +1,28 @@
 module Main where
 
-import Data.Void ( Void )
-import Text.Megaparsec ( parse, errorBundlePretty, many, sepBy, Parsec )
-import Text.Megaparsec.Char ( string, char, eol )
+import Data.Void (Void)
+import Text.Megaparsec (Parsec, errorBundlePretty, many, parse, sepBy)
+import Text.Megaparsec.Char (char, eol, string)
 import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void String
 
-data Equation = Equation {
-    res :: Int,
+data Equation = Equation
+  { res :: Int,
     numbers :: [Int]
-} deriving Show
+  }
+  deriving (Show)
 
 equationParser :: Parser Equation
 equationParser = Equation <$> L.decimal <* string ": " <*> L.decimal `sepBy` char ' '
-
-test :: Parser Int
-test = L.decimal
 
 inputParser :: Parser [Equation]
 inputParser = many (equationParser <* eol)
 
 main :: IO ()
 main = do
-    input <- readFile "test-input.txt"
-    let x = parse inputParser "" input
-    case x of
-        Left err -> putStrLn $ "Error: " ++ errorBundlePretty err
-        Right n  -> mapM_ print n
-    
+  input <- readFile "test-input.txt"
+  let x = parse inputParser "" input
+  case x of
+    Left err -> putStrLn $ "Error: " ++ errorBundlePretty err
+    Right n -> mapM_ print n
