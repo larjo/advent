1bf5116b34a3a17138730e9c6ec79abf7a6cbb1d
Lars <lars.baker@uc.se>
2025-02-01 00:14:31 +0100

    Done part 1

--- 2024/a07/app/Main.hs
+++ 2024/a07/app/Main.hs
@@ -1,28 +1,44 @@
 module Main where
 
+import Data.Either (fromRight)
+import Data.List (find)
+import Data.Maybe (mapMaybe)
 import Data.Void (Void)
-import Text.Megaparsec (Parsec, errorBundlePretty, many, parse, sepBy)
+import Text.Megaparsec (Parsec, many, parse, sepBy)
 import Text.Megaparsec.Char (char, eol, string)
 import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void String
 
 data Equation = Equation
-  { res :: Int,
+  { result :: Int,
     numbers :: [Int]
   }
   deriving (Show)
 
+numberListParser :: Parser [Int]
+numberListParser = L.decimal `sepBy` char ' '
+
 equationParser :: Parser Equation
-equationParser = Equation <$> L.decimal <* string ": " <*> L.decimal `sepBy` char ' '
+equationParser = Equation <$> L.decimal <* string ": " <*> numberListParser
 
 inputParser :: Parser [Equation]
 inputParser = many (equationParser <* eol)
 
+calc1 :: Int -> [Int] -> [Int]
+calc1 n acc = [op a n | a <- acc, op <- [(*), (+)]]
+
+calcs :: [Int] -> [Int]
+calcs [] = []
+calcs (a : ns) = foldr calc1 [a] . reverse $ ns
+
+testEq :: Equation -> Maybe Int
+testEq eq = find (== result eq) . calcs . numbers $ eq
+
 main :: IO ()
 main = do
-  input <- readFile "test-input.txt"
-  let x = parse inputParser "" input
-  case x of
-    Left err -> putStrLn $ "Error: " ++ errorBundlePretty err
-    Right n -> mapM_ print n
+  input <- readFile "input.txt"
+  let res = parse inputParser "" input
+  let eqs = fromRight [] res
+  putStr "Part 1: "
+  print $ sum . mapMaybe testEq $ eqs
