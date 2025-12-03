efb727e2aef04cbee6422aaaefd51f4848ea42c5
Lars <lars.baker@uc.se>
2025-12-03 22:27:43 +0100

    Part 1 done

--- 2025/a01/app/Main.hs
+++ 2025/a01/app/Main.hs
@@ -1,4 +1,38 @@
 module Main where
 
+import Data.Either (fromRight)
+import Data.Functor (($>))
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void String
+
+testPuzzle :: String
+testPuzzle = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"
+
+data Direction = L | R deriving (Show)
+
+data Turn = Turn {direction :: Direction, distance :: Int} deriving (Show)
+
+turn :: Int -> Turn -> Int
+turn pos (Turn L dist) = (pos - dist) `mod` 100
+turn pos (Turn R dist) = (pos + dist) `mod` 100
+
+directionParser :: Parser Direction
+directionParser = (char 'L' $> L) <|> (char 'R' $> R)
+
+turnParser :: Parser Turn
+turnParser = do
+  dir <- directionParser
+  Turn dir <$> L.decimal
+
+puzzleParser :: Parser [Turn]
+puzzleParser = turnParser `sepBy` newline
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  input <- readFile "input.txt"
+  let turns = fromRight [] . parse puzzleParser "" $ input
+  print $ length . filter (== 0) . scanl turn 50 $ turns
