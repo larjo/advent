ec03263f7287d3d85b2875c25ee055de11a05668
Lars <lars.baker@uc.se>
2025-12-03 22:43:50 +0100

    Golfing

--- 2025/a01/app/Main.hs
+++ 2025/a01/app/Main.hs
@@ -1,32 +1,22 @@
 module Main where
 
 import Data.Either (fromRight)
-import Data.Functor (($>))
 import Data.Void
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Megaparsec.Char.Lexer
 
 type Parser = Parsec Void String
 
-testPuzzle :: String
-testPuzzle = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82"
-
-data Direction = L | R deriving (Show)
-
-data Turn = Turn {direction :: Direction, distance :: Int} deriving (Show)
+data Turn = Turn {direction :: Char, distance :: Int} deriving (Show)
 
 turn :: Int -> Turn -> Int
-turn pos (Turn L dist) = (pos - dist) `mod` 100
-turn pos (Turn R dist) = (pos + dist) `mod` 100
-
-directionParser :: Parser Direction
-directionParser = (char 'L' $> L) <|> (char 'R' $> R)
+turn pos (Turn 'L' dist) = (pos - dist) `mod` 100
+turn pos (Turn 'R' dist) = (pos + dist) `mod` 100
+turn _ _ = error "invalid direction"
 
 turnParser :: Parser Turn
-turnParser = do
-  dir <- directionParser
-  Turn dir <$> L.decimal
+turnParser = Turn <$> anySingle <*> decimal
 
 puzzleParser :: Parser [Turn]
 puzzleParser = turnParser `sepBy` newline
